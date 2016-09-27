1b85d61f0105854f37e8b566aa775fc1b4aab5ce
Aengus Tran <aengustran@gmail.com>
2016-09-27 15:28:48 +1000

    4th Commit: Browser View Controller Finished

---
+++ Pokedex3/DetailController.swift
@@ -0,0 +1,35 @@
+//
+//  PokemonDetailController.swift
+//  Pokedex3
+//
+//  Created by Aengus Tran on 27/9/16.
+//  Copyright © 2016 Aengus Tran. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
