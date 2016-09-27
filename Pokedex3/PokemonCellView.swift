d75c76de5d7abfc83835b1a4986d313294481e96
Aengus Tran <aengustran@gmail.com>
2016-09-27 10:30:55 +1000

    2nd Commit - Added collection view and search bar

---
+++ Pokedex3/PokemonCellView.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonCellView.swift
+//  Pokedex3
+//
+//  Created by Aengus Tran on 27/9/16.
+//  Copyright © 2016 Aengus Tran. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCellView: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Pokedex3/PokemonModel.swift
@@ -0,0 +1,35 @@
+//
+//  PokemonModel.swift
+//  Pokedex3
+//
+//  Created by Aengus Tran on 27/9/16.
+//  Copyright © 2016 Aengus Tran. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexID: Int!
+
+    var name: String {
+
+
+        return _name
+    }
+
+
+    var pokedexID: Int {
+
+
+
+        return _pokedexID
+    }
+
+
+    init(name: String, pokedexID: Int) {
+        self._name = name
+        self._pokedexID = pokedexID
+    }
+
+}
